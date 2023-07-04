e5e97fb55eab2a97b31fbc86824b7c808fb3c7fd
RyanWalck <rwalck33@gmail.com>
2023-07-04 17:23:17 -0400

    super user creation

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Pokemon # import the Artist model from models.py
 # Register your models here.
+
+admin.site.register(Pokemon) # this line will add the model to the admin panel
